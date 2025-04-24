5b101d01cc689b82d3705e183676c2c921e7e459
Alaa Haamdi <alaahaamdii@gmail.com>
2025-04-24 17:20:12 +0200

    add structurel directive and newer @for

--- src/app/child/child.component.ts
+++ src/app/child/child.component.ts
@@ -1,22 +1,31 @@
 import {Component, EventEmitter, Input, Output} from '@angular/core';
 
 @Component({
-  selector: 'app-child',
-  imports: [],
-  template: `
-    <div>
-      <p>Child Component</p>
-      <p>Received from parent: {{ inputData }}</p>
-      <button (click)="sendData()">Send Data to Parent</button>
-    </div>
-  `,
-  standalone: true,
-  styles: ``
+    selector: 'app-child',
+    imports: [],
+    template: `
+        <div style="border: 1px solid #ccc; padding: 10px; margin-top: 10px;">
+            <p>List</p>
+            <ul>
+                <li *ngFor="let item of items">{{ item.name }}</li>
+            </ul>
+            <p>Child Component</p>
+            <p>Received from parent: {{ inputData }}</p>
+            <button (click)="sendData()">Send Data to Parent</button>
+        </div>
+    `,
+    standalone: true,
+    styles: ``
 })
 export class ChildComponent {
     @Input() inputData: string = ''; // Input property to receive data from parent
     @Output() outputData = new EventEmitter<string>(); // Output property to send data to parent
-
+    items: { id: number; name: string }[] = [
+        { id: 1, name: 'Apple' },
+        { id: 2, name: 'Banana' },
+        { id: 3, name: 'Cherry' },
+        { id: 4, name: 'Date' }
+    ];
     sendData() {
         this.outputData.emit('Hello from Child!'); // Emit an event with data
     }
--- src/app/child/new-child.component.ts
+++ src/app/child/new-child.component.ts
@@ -10,6 +10,12 @@ import {
     standalone: true,
     template: `
         <div style="border: 1px solid #ccc; padding: 10px; margin-top: 10px;">
+            <p>List</p>
+            <ul>
+                @for (item of items; track item.id) {
+                <li>{{ item.name }}</li>
+                }
+            </ul>
             <p>Child Component</p>
             <p>Received from parent: {{ inputData() }}</p>
             <button (click)="sendBack()">Send Data to Parent</button>
@@ -19,7 +25,12 @@ import {
 export class NewChildComponent {
     inputData = input<string>(); // functional version of @Input()
     outputData = output<string>(); // functional version of @Output()
-
+    items: { id: number; name: string }[] = [
+        { id: 1, name: 'Apple' },
+        { id: 2, name: 'Banana' },
+        { id: 3, name: 'Cherry' },
+        { id: 4, name: 'Date' }
+    ];
     sendBack() {
         this.outputData.emit('Hello from Child!');
     }
